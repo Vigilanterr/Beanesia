745bd23372f37647abf5e32026cec7d45a19e218
Vigilante <tiger191110@gmail.com>
2026-05-06 18:16:05 +0700

    Update

--- src/dist/include.js
+++ src/dist/include.js
@@ -1,5 +1,11 @@
-        fetch('/src/Components/Navbar.html')
-            .then(response => response.text()) 
-            .then(data => {
-                document.getElementById('navbar-container').innerHTML = data;
-            })
+fetch('/src/Components/Navbar.html')
+  .then(res => res.text())
+  .then(data => {
+    document.getElementById('navbar-container').innerHTML = data;
+  });
+
+fetch('/src/Components/Hero.html')
+  .then(res => res.text())
+  .then(data => {
+    document.getElementById('hero-container').innerHTML = data;
+  });
