dc3e2071bb7faa7e2d5c327bd8c7ba1644e02f7d
Vigilante <tiger191110@gmail.com>
2026-05-07 07:48:06 +0700

    Update Login

--- src/dist/Log/auth.js
+++ src/dist/Log/auth.js
@@ -3,7 +3,7 @@ async function login() {
   const password = document.getElementById("password").value;
 
   try {
-    let response = await fetch("/src/dist/user.json");
+    let response = await fetch("/src/data/user.json");
     let users = await response.json();
 
     let user = users.find(u => u.email === email && u.password === password);
@@ -11,12 +11,13 @@ async function login() {
     if (user) {
       sessionStorage.setItem("user", JSON.stringify(user));
       alert("Login berhasil!");
-      window.location.href = "index.html";
+      window.location.href = "/src/pages/home.html"; // ← diubah
     } else {
       alert("Email atau password salah!");
     }
   } catch (error) {
     console.log("Error:", error);
+    alert("Terjadi kesalahan saat login. Coba lagi."); 
   }
 }
 
@@ -24,13 +25,15 @@ function checkLogin() {
   const user = sessionStorage.getItem("user");
 
   if (user) {
-    window.location.href = "index.html";
+    window.location.href = "/src/pages/home.html"; 
   }
 }
 
 function logout() {
   sessionStorage.removeItem("user");
-  window.location.href = "login.html";
+  window.location.href = "/src/pages/Login.html"; 
+  console.log("test");
+  
 }
 
 checkLogin();
